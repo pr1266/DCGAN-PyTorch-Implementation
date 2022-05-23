3ed065d18b19bcd86b3dce4d25e907856999cbc5
pourya <pourya_1999@yahoo.com>
2022-05-24 01:50:56 +0430

    'train_imports'

---
+++ train.py
@@ -0,0 +1,10 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision
+import torchvision.datasets as datasets
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from model import Discriminator, Generator, initialize_weights
+
