53836c5cfef9f20563775e0b029cc5796e1fca20
pourya <pourya_1999@yahoo.com>
2022-05-24 00:43:39 +0430

    'disc'

--- model.py
+++ model.py
@@ -1,2 +1,16 @@
 import torch
 import torch.nn as nn
+
+class Discriminator(nn.Module):
+
+    def __init__(self, channels_img, features_d):
+        super(Discriminator, self).__init__()
+        self.disc = nn.Sequential(
+            nn.Conv2d(channels_img, features_d, kernel_size=4, stride=2, padding=1),
+            )
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(0.2)
+        )
