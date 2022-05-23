c510a25dbeca89d9692962778913d72e2f919902
pourya <pourya_1999@yahoo.com>
2022-05-24 01:24:26 +0430

    'gen_g'

--- model.py
+++ model.py
@@ -30,3 +30,20 @@ class Generator(nn.Module):
 
     def __init__(self, z_dim, channels_img, features_g):
         super(Generator, self).__init__()
+        self.net = nn.Sequential(
+            #! input shape: Batch size * z_dim * 1 * 1
+            self._block(z_dim, features_g*16, 4, 1, 0), #! N * f_g * 16 * 4 * 4
+            self._block(features_g*16, features_g*8, 4, 2, 1), #! 8 * 8
+            self._block(features_g*8, features_g*4, 4, 2, 1), #! 16 * 16
+            self._block(features_g*4, features_g*2, 4, 2, 1), #! 32 * 32
+            nn.ConvTranspose2d(features_g*2, channels_img, kernel_size=4, stride=2, padding=1), #! 64 * 64
+            nn.Tanh(), #! [-1, 1]
+        )
+
+    def _block(self, in_channels, out_channels, kernel_size, stride, padding):
+        return nn.Sequential(
+            nn.ConvTranspose2d(in_channels, out_channels, kernel_size, stride, padding, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+        )
+
